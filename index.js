00919e9a234f80242f4ee1e733cbab04a7e9c1f2
Joey <joeyhightower@Joeys-MBP.bak.rr.com>
2022-10-27 11:37:59 -0700

    Second Draft

--- index.js
+++ index.js
@@ -6,28 +6,50 @@ searchBar.addEventListener("input", (event) =>{
 
 })
 
+
+let searchBookmarks = document.querySelector("#searchBookmark")
+
+function filterBookmarks(searchBookmarkInput){
+    searchBookmarkInput = searchBar.value.includes(searchBookmarks.value)
+    return searchBookmarkInput
+}
+
+searchBookmarks.addEventListener("input", (event) =>{
+    let searchBookmarkValue = searchBookmarks.value.toLowerCase();
+    console.log("this is inputVal", searchBookmarkValue)
+    
+})
+
+
+
 class Bookmarks{
-    constructor(bookmark){
-        this.bookmark = bookmark;
+    constructor(){
+        this.bookmarks = []
     }
     render(){
         const bookmarkList = document.querySelector(".list")
-        const listItem = document.createElement("a")
-        listItem.textContent = searchBar.value
-        listItem.href = searchBar.value
-        bookmarkList.append(listItem)
+        bookmarkList.innerHTML = ''
+        let x = this.bookmarks
+        x.filter(filterBookmarks)
+        for(let bookmark of x){
+            const listItem = document.createElement("a")
+            listItem.textContent = bookmark
+            listItem.href = bookmark
+            bookmarkList.append(listItem)
+        }
+    }
+
+    addBookmark(){
+        this.bookmarks.push(searchBar.value)
+        console.log(this.bookmarks)
     }
 }
 
-
-let b = new Bookmarks(searchBar)
+let b = new Bookmarks()
 
 let addBookmark = document.querySelector("button")
 addBookmark.addEventListener("click", function(){
+    b.addBookmark()
+    searchBar.value = ''
     b.render()
-})
-
-searchBookmark.addEventListener("input", (event) =>{
-    
-
 })
